dc47fb0accccd2a073c0f841abbfd09f230a4579
kuruwata <109745695+kuruwata@users.noreply.github.com>
2023-10-02 15:21:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,4 +7,24 @@ const text = document.getElementById('bodyText');
 
 //document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
 
-text.textContent = color.value;
+function changeColor() {
+    // カラーコードを表示
+    if (color.value === '#ffffff') {
+        text.textContent = 'コード：' + color.value + '(white)';
+    } else if (color.value === '#000000') {
+        text.textContent = 'コード：' + color.value + '(black)';
+    } else if (color.value === '#ff0000') {
+        text.textContent = 'コード：' + color.value + '(red)';
+    } else if (color.value === '#0000ff') {
+        text.textContent = 'コード：' + color.value + '(blue)';
+    } else if (color.value === '#ffff00') {
+        text.textContent = 'コード：' + color.value + '(yellow)';
+    } else {
+        text.textContent = 'コード：' + color.value;
+    }
+    // 背景色を変更
+    document.body.style.backgroundColor = color.value;
+}
+
+// カラーピッカーで色が選択されたら、changeColorを動作させる
+color.addEventListener('input', changeColor);
